2f412f4544c7a0cccaddb66641ac6004644b7972
Lewis <lewisvanhouten@outlook.com>
2018-05-12 22:04:02 -0700

    Day 11
    Java solution for the Day 11 challenge.

---
+++ Day11.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        scanner.close();
+        printMaxHourglass(arr);
+    }
+  
+    private static void printMaxHourglass(int[][] hourglasses){    
+      int[] totals    = new int[16];
+      int totalsIndex = 0;
+      
+      for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+          totals[totalsIndex] = hourglasses[i][j] + hourglasses[i][j+1] + hourglasses[i][j+2] + hourglasses[i+1][j+1] + hourglasses[i+2][j] + hourglasses[i+2][j+1] + hourglasses[i+2][j+2];
+            
+          totalsIndex++;
+        }
+      }
+      Arrays.sort(totals);
+      System.out.println(totals[totals.length - 1]);
+    }
+  
+}
